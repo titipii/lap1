fa121074c249243c22ce21eec183313951d3217b
Edgar <titipii21@gmail.com>
2017-11-29 09:29:18 +0000

    hubba

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
- http_basic_authenticate_with name: "npname", password: "it231"
+ http_basic_authenticate_with name: "hubba", password: "bubba"
 end
