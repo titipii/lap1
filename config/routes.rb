f0b84d421a4831a5600f2dd3fb0d48197a9020c8
Edgar <titipii21@gmail.com>
2017-11-29 07:47:37 +0000

    Finish home pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  root 'my_pages#home'
+  
+  get 'my_pages/home'
+
+  get 'my_pages/help'
+
+  get 'my_pages/about'
+
   resources :microposts
 resources :users
-root 'users#index'
 end
